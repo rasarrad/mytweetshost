7bead82bebd5ebda3ead5bf64abc81c8a045f8d4
rasarrad <rasarrad@gmail.com>
2019-09-03 22:26:07 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var dosearchmore = true;
 var url = "";
 var dblFlag = false;
 var dblClickTimeout = null;
-console.log(2222); 
+console.log(33333); 
 
 $( document ).ready(function() {
   var hasChanges = readCookie("hasChanges");
