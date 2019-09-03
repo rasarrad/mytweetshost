ff3ce299ebbc89c2fe403a61959205f4bc066192
rasarrad <rasarrad@gmail.com>
2019-09-03 23:01:01 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var dosearchmore = true;
 var url = "";
 var dblFlag = false;
 var dblClickTimeout = null;
-console.log(4444); 
+console.log(1111); 
 
 $( document ).ready(function() {
   var hasChanges = readCookie("hasChanges");
