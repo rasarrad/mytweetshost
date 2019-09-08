57153a8a64c2385a26fe1b094b0c4fd7d6e59faa
rasarrad <rasarrad@gmail.com>
2019-09-08 13:45:58 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var dosearchmore = true;
 var url = "";
 var dblFlag = false;
 var dblClickTimeout = null;
-console.log(2222222); 
+console.log(3333); 
 
 $( document ).ready(function() {
   var hasChanges = readCookie("hasChanges");
@@ -1203,6 +1203,12 @@ $(document).keydown(function(e) {
     } 
 }); */
 
-function aaaas() {
-  alert('double click');
+function countmove(obj) {
+  if ($(obj).css('top') == "67px") {
+    
+    $(obj).css('top', 'calc(100% - 142px)');
+  }
+  else {
+    $(obj).css('top', '67px');
+  }
 }  
