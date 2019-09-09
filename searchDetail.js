0ffaf612277fb2ee133c7d168999c58458317713
rasarrad <rasarrad@gmail.com>
2019-09-09 10:21:36 +0100

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -1,1 +1,64 @@
-----
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+
+var togglecriterions = function() {
+    if ($('.toptitle').css('display') == 'none') {
+        $(".top").css("transition", "all 0.7s");
+        $('.top').css('opacity', '0');
+        
+        setTimeout(function(){
+            $('html').find('.top').each( function( index, element ){
+                $(this).css('display', 'none');
+            });
+            
+            $(".toptitle").css("transition", "none");
+            $('.toptitle').css('opacity', '1');
+            $('.toptitle').fadeIn(500);
+        }, 400);
+    }
+    else {
+        $(".toptitle").css("transition", "opacity 0.7s");
+        $('.toptitle').css('opacity', '0');
+        setTimeout(function(){
+            $('.toptitle').css('display', 'none');
+            $(".top").css("transition", "none");
+            $('.top').css('opacity', '1');
+            $('html').find('.top').each( function( index, element ){
+            $(this).fadeIn(500);
+            
+            });
+            setTimeout(function(){
+            $(".top").css("transition", "all 0.7s");
+            }, 1400);
+        }, 400);
+    } 
+}   
+
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+
+function resetFields(flag) {
+    $("#main").empty();
+    $('#moretweets').hide();
+    $('#selectedcat').val('all');
+    $('#selectedcattext').val('All Tweets');
+    $('#tweetcount').hide();  
+    $('#filtertext').val('');
+    $('#filterdate1').val('');
+    $('#filterdate2').val('');
+    $('#filterid').val('');
+    $('#filterauthor').val('');
+    $('#filtertag').val('');
+
+    if (flag) 
+        showMessage("Search Criterions Cleaned"); 
+} 
+
