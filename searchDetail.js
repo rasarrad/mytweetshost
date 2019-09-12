bcef93ab90075dd8f3d664c7e755411d9fa73e17
rasarrad <rasarrad@gmail.com>
2019-09-12 20:42:16 +0100

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -122,7 +122,7 @@ var getInformation = function(ismoretweets) {
                 console.log("dooooo");
                 var linktmp = decodeURIComponent(readCookie(nextid + "templink"));
                 
-                linktmp = linktmp.substring(0, linktmp.length - 2); 
+                linktmp = linktmp.substring(1, linktmp.length - 2); 
                 console.log(888888);
                 console.log(linktmp);
                 linktmp = JSON.parse(linktmp);
