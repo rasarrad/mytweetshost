ea8bb4ad55fc61b1693ba9a8daea74f849e5b91c
rasarrad <rasarrad@gmail.com>
2019-09-21 19:09:26 +0100

    aaaaaaaa

--- add.js
+++ add.js
@@ -120,7 +120,7 @@ function create() {
     } 
     var resinfo = $('#info').val().replace(/"/g, "");
     resinfo = resinfo.replace(/(\r\n|\n|\r)/gm, "");
-
+alert(nextid);
     if (addType == "T") {
         $('#result').val("{\r\n\"id\": \"" + nextid + "\",\r\n\"type\": \"" + addType  + "\",\r\n\"url\": \"" + url  + "\",\r\n\"ishidden\": \"" + ishidden  + "\",\r\n\"date\": \"" + $('#date').val() + "\",\r\n\"author\": \"" + origin  + "\",\r\n\"categories\": \"" + $('#categories').val() + "\",\r\n\"tags\": \"" + $('#tags').val() + "\",\r\n\"info\": \"" + resinfo + "\",\r\n\"classif\": \"" + $('#classifpop').val() + "\",\r\n\"tweet\": " + text + "\r\n},");
     }
@@ -151,6 +151,7 @@ function create() {
     if ($("#preview").is(":checked")) {
         createCookie(nextid + "templink", encodeURIComponent(JSON.stringify(result)), 99999);
         createCookie("hasChanges", "Yes");
+        $("#generate").addClass("haschanges");
 /*         var tmpid = readCookie("tmpid");
         if (tmpid && tmpid.length > 0) {
 
