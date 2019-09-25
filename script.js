1885c7846d0c6bc34e9ccfe5b2aeffc085d23d4c
rasarrad <rasarrad@gmail.com>
2019-09-25 10:14:10 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-console.log(111); 
+console.log(333); 
 
 var text = "";
 var origin = "";
@@ -14,7 +14,7 @@ var dblClickTimeout = null;
 var addType = "T";
 
 $( document ).ready(function() {
-    $(body).css("height", ($(window).height() - 100) + "px");
+    $("body").css("height", ($(window).height() - 100) + "px");
     countalltweets();
     var hasChanges = readCookie("hasChanges");
     if (hasChanges && hasChanges.length > 0)
