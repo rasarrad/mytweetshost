c6d559fd3eedee426aaecb5c9ee5a5efa7a83d3b
rasarrad <rasarrad@gmail.com>
2019-10-14 11:58:09 +0100

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -668,7 +668,7 @@ var getInformationOld = function(ismoretweets) {
                 objToFocus = currentIndex;
                 var newtweetobjaction = newtweetobj;
                 $('html, body').animate({
-                scrollTop: $(newtweetobjaction).offset().top - 160 
+                scrollTop: $(newtweetobjaction).offset().top - 260 
                 }, 100);
 
             }
