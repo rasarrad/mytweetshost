cebbbdac0fe5cd0f9558e695786c996a263b78b3
rasarrad <rasarrad@gmail.com>
2019-10-30 09:04:47 +0000

    aaaaaaaa

--- settings.js
+++ settings.js
@@ -1,1 +1,23 @@
 
+
+function expandCat(obj) {
+    openSettingsPopup($(obj).parent());
+}
+
+var openSettingsPopup = function(flag) 
+{
+    var json = getJsonbyid(obj.attr("id"));
+
+    if (json) {
+        console.log(json);
+        $('#linkChange').fadeIn();
+    }
+} 
+
+function closeSettingsPopup(obj) {
+    $('#linkChange').fadeOut();
+}
+
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
