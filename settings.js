e45b6a4c3fb82bc5aa21c79fd628537da8ee1bfb
rasarrad <rasarrad@gmail.com>
2019-11-05 22:07:01 +0000

    aaaaaaaa

--- settings.js
+++ settings.js
@@ -105,7 +105,7 @@ var openSettingsPopup = function(jsonobj)
     markCategoriesCheckBoxs();
 
     // CLASSIFICATION
-
+alert(jsonobj.classif)
     $('#classifinput').attr("cclassif", jsonobj.classif);
 
     var classifchanged = readCookie(jsonobj.id + "classif");
