f4bb0be892ee4f971f87ba48eb41854e03c56030
rasarrad <rasarrad@gmail.com>
2019-11-19 10:04:32 +0000

    aaaaaaaa

--- settings.js
+++ settings.js
@@ -423,10 +423,12 @@ function editSetting(e, obj) {
         $('#linkChange').find("table:not(.buttonstable)").each( function( index, element ) {
             var table = $(element);
             
-            table.css('transition', 'max-height 0.01s');
+            table.css('transition', 'max-height ease-out 1.99s');
             table.css('max-height', setHeight);
             table.find('.sectionedittd i').addClass('fa-edit').removeClass('fa-angle-up').attr('style', '');
-            table.find('td.el').addClass('ellipsis');
+            setTimeout(function() { 
+                table.find('td.el').addClass('ellipsis');
+            }, 1500);
         });
     
         table.css('transition', 'max-height 6.3s');
@@ -437,7 +439,7 @@ function editSetting(e, obj) {
   
     }
     else {
-        table.css('transition', 'max-height ease-out 2.99s');
+        table.css('transition', 'max-height ease-out 1.99s');
         table.css('max-height', setHeight);
         table.find('.sectionedittd i').addClass('fa-edit').removeClass('fa-angle-up').attr('style', '');
         setTimeout(function() { 
