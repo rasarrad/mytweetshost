430cf7ae0c93825f92d1894d0054a52d810e12c2
rasarrad <rasarrad@gmail.com>
2019-11-21 21:38:14 +0000

    aaaaaaaa

--- add.js
+++ add.js
@@ -212,7 +212,8 @@ function createPreview() {
         
     if (addType == "T") {
         newtweetobj.append($('<div class="innertweet"></div>'));
-        newtweetobj.find('.innertweet').append(text.substring(1, text.length -1));
+        newtweetobj.find('.innertweet').append(text.substring(1, text.length -1))
+        customizeTweets(null, true, null, 1);
     }
     else {
         newtweetobj.append(text.substring(1, text.length -1));
