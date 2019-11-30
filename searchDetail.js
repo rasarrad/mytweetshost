218d7841b0101570eeb4a68ba7e353c151ec8d25
rasarrad <rasarrad@gmail.com>
2019-11-30 20:31:22 +0000

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -30,7 +30,7 @@ function changecriteria(e, obj) {
         table.find('.sectionedittd i').addClass('fa-angle-up').removeClass('fa-edit').attr('style', 'font-size: 22px;position: relative;margin-top: -4px;');
 
         table.find('td.el').removeClass('ellipsis');
-
+alert(1111)
         $("#searchpopup > div").attr("style", "max-height: " + (124 + Number(table.attr("cmaxheight"))) + "px !important;margin-top: " + (112 + Number(table.attr("cmargin"))) + "px !important;")
     }
     else {
