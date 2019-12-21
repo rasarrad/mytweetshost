879151fac2062def6ee7cc332356804fd03f3e9c
rasarrad <rasarrad@gmail.com>
2019-12-21 19:03:49 +0000

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -1345,7 +1345,8 @@ var countalltweets = function(id) {
                  */
                 var elem = $("<li  onclick='javascript: clickLiTag(event, this)' class='litags'>" + key + "</li>");
                 $("#tagsul").append(elem);
-                $('#tagsearchul').append(elem);
+                var elem2 = $("<li  onclick='javascript: clickLiTag(event, this)' class='litags'>" + key + "</li>");
+                $('#tagsearchul').append(elem2);
             }  
     
             $("#addpopup").hide();
