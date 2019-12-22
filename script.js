21ecbfe55d7bde9e6536636e285040b1d830f683
rasarrad <rasarrad@gmail.com>
2019-12-22 16:30:09 +0000

    aaaaaaaa

--- script.js
+++ script.js
@@ -25,10 +25,12 @@ var calendar = null;
 var filterdate1date = null;
 var filterdate2date = null;
 
-
+setTimeout(function(){
+    $("#mask").css("background-image", "linear-gradient(to bottom, red 4%, green 126%)");
+}, 40); 
 
 $( document ).ready(function() { 
-    $("#mask").css("background-image", "linear-gradient(to bottom, red 4%, green 126%)");
+    
     console.log('-------------- app - BEGIN --------------');
     /*$( "#mask" ).fadeOut( 1100, function() {
         var style = window.getComputedStyle(body, null);
