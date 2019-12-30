ee55088b9d2bfd31a225e527d8f0621796f76482
rasarrad <rasarrad@gmail.com>
2019-12-30 11:43:49 +0000

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -1594,14 +1594,7 @@ var countalltweets = function(id) {
         dblFlag = false;  
 
         if (dosearchmore) {
-            $( "#mask" ).fadeOut( 800, function() {
-                var style = window.getComputedStyle(body, null);
-        
-                $("#mask").css("background", style.getPropertyValue('--soft-transp-color'));
-                $("#mask .fa-folder-open").hide();
-                $("#mask > div" ).hide();
-                $("#mask > .fa-circle-o-notch").show();
-            });
+
         }
     }); 
 
