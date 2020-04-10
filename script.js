8a7edb64e9178a9c069206224e99d5a6c98f2b31
rasarrad <rasarrad@gmail.com>
2020-04-10 10:35:00 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -363,7 +363,7 @@ function executeSwipeFunction(obj, type) {
         isLink = false;
     }
 console.log(isLink);
-    if (isLink) {
+    if (idLink != "NaN") {
         processLinkFuncs(idLink, type);
     }
     else {
