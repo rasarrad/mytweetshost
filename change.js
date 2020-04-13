1514b67d511438c263f4263942e2c635d05f3db9
rasarrad <rasarrad@gmail.com>
2020-04-13 18:27:10 +0100

    aaaaaaaa

--- change.js
+++ change.js
@@ -1148,7 +1148,7 @@ function generate(obj) {
         
             var blob = new Blob([text],
             { type: "text/plain;charset=utf-8" });
-                saveAs(blob, "BookmarksStationLinks.txt");
+                saveAs(blob, "BookmarksStationLinks.txt", null);
             }
     }); 
 } 
