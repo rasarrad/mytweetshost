cac5278f70b0376558f6aab0a8fe84d184798b7f
rasarrad <rasarrad@gmail.com>
2020-04-14 18:44:44 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -726,7 +726,7 @@ function handleFileSelect(evt) {
   
   function download(filename, text) {
     var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('href', 'data:text/plain,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
   
     element.style.display = 'none';
