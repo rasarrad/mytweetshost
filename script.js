a80442ad22832ecd71d865d4946ea70f41be590c
rasarrad <rasarrad@gmail.com>
2020-04-15 15:17:14 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -629,28 +629,30 @@ $( document ).ready(function() {
         })
     });
     document.getElementById("folderopen").addEventListener("touchstart", tapHandler);
-    document.getElementById("folderopen").addEventListener("click", clickTapHandler);
+    document.getElementById("folderopen").addEventListener("click", tapHandler);
     
 }); // FIM DO ONREADY
 
 var dblFlagControl = true;
 function tapHandler(event) {
-    dblFlagControl = false;
-    if(!dblFlag) {
-        dblFlag = true;
-        dblClickTimeout = setTimeout( function() { 
-            dblFlag = false; 
-            console.log('ONCE');
-            dblFlagControl = true;
-        }, 250 );
-        return false;
+    if (dblFlagControl || dblFlag) {
+        dblFlagControl = false;
+        if(!dblFlag) {
+            dblFlag = true;
+            dblClickTimeout = setTimeout( function() { 
+                dblFlag = false; 
+                console.log('ONCE');
+                dblFlagControl = true;
+            }, 250 );
+            return false;
+        }
+        event.preventDefault();
+        
+        clearTimeout(dblClickTimeout);
+        dblFlag = false;
+        console.log('You tapped me Twice !!!');
+        dblFlagControl = true;
     }
-    event.preventDefault();
-    
-    clearTimeout(dblClickTimeout);
-    dblFlag = false;
-    console.log('You tapped me Twice !!!');
-    dblFlagControl = true;
  }
 
  function clickTapHandler(event) {
