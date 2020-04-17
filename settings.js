99cb197c32a4857ccb41860616bbc48d7bda2e27
rasarrad <rasarrad@gmail.com>
2020-04-17 21:50:35 +0100

    aaaaaaaa

--- settings.js
+++ settings.js
@@ -969,7 +969,7 @@ function updateLinkColor(val, id) {
         console.log(val.id + "-" + isdeleted + "-" + val.deleted)
         if (val.deleted != "" || (isdeleted && isdeleted.length > 0)) { 
             if (showColors) {
-                console.log($("#" + id)
+                console.log("#" + id)
                 console.log($("#" + id).find("i.linkbar"))
                 $("#" + id).find("i.linkbar").css("color", "red"); 
                 $("#seticon").attr("style", "color: red;");
