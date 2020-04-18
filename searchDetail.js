671b676790b37338983396ed0ae8eded7c1cd953
rasarrad <rasarrad@gmail.com>
2020-04-18 10:01:33 +0100

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -341,7 +341,7 @@ function calendarChanged(date) {
                     $("#linkChange").find(".dateinput").val(formatNumDate(date));
                     
                     console.log("-" + formatNumDate(date) + "-" + $('#postedby').attr("cauthor") + "-")
-                    if (formatNumDate(date) != $('#postedby').attr("cauthor")) {
+                    if (formatNumDate(date) != $('#date').attr("cdate")) {
                         createCookie($('#linkChange').attr("cid") + "datechanged", formatNumDate(date));
                         createCookie($('#linkChange').attr("cid") + "haschanges", "yes");
                         if (showColors) {
