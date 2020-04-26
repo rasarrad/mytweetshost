ce9d39d38177807fe756dfe866cce65a94053bfc
rasarrad <rasarrad@gmail.com>
2020-04-26 12:53:25 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ if (currTheme && currTheme.length > 0 && currTheme != 'default') {
 
 $( document ).ready(function() { 
 
-    hasAvailableImage('45', 'https://s.wordpress.com/mshots/v1/https://smallwarsjournal.com/jrnl/art/victimization-narrative-thematic-analysis-iranian-history-and-strategy')
+    hasAvailableImage('45', 'https://s.wordpress.com/mshots/v1/https://ZZZsmallwarsjournal.com/jrnl/art/victimization-narrative-thematic-analysis-iranian-history-and-strategy')
 
     // START do texto das categorias
     var catschanged = readCookie("cat-cli");
