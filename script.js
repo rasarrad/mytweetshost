456646181471e319e619a52b9149ec89dba6f0d6
rasarrad <rasarrad@gmail.com>
2020-04-27 11:29:59 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -53,6 +53,24 @@ function hasAvailable(id, url) {
             console.log('-------------------------------');
             console.log(result.substring(result.indexOf(",\"title\":\"") + 10, result.indexOf("\",\"lengthSeconds\"")).replace(/\+/g, ' '));
             console.log(result.substring(result.indexOf("\",\"lengthSeconds\":\"") + 19, result.indexOf("\",\"keywords\":")).replace(/\+/g, ' '));
+
+            let totalSeconds = parseInt(result.substring(result.indexOf("\",\"lengthSeconds\":\"") + 19, result.indexOf("\",\"keywords\":")).replace(/\+/g, ' '));
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+
+console.log("hours: " + hours);
+console.log("minutes: " + minutes);
+console.log("seconds: " + seconds);
+
+// If you want strings with leading zeroes:
+minutes = String(minutes).padStart(2, "0");
+hours = String(hours).padStart(2, "0");
+seconds = String(seconds).padStart(2, "0");
+console.log(hours + ":" + minutes + ":" + seconds);
+
+
       }
     }).always(function(jqXHR, textStatus) {
     });
