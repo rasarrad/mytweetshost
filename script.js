04955a611ace3018c805bf20a08ed2564d280b39
rasarrad <rasarrad@gmail.com>
2020-04-29 21:37:30 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ $( document ).ready(function() {
 
     //hasAvailableImage('45', 'https://cors-anywhere.herokuapp.com/https://s.wordpress.com/mshots/v1/https://smallwarsjournal.com/jrnl/art/victimization-narrative-thematic-analysis-iranian-history-and-strategy/')
 
-    getWebsiteData2('https://www.codementor.io/blog/senior-developer-interview-questions-6on7cl3reg#senior-software-engineer-interview-questions')
+    getWebsiteData2('https://smallwarsjournal.com/jrnl/art/victimization-narrative-thematic-analysis-iranian-history-and-strategy')
 
     isMobile = window.mobileAndTabletCheck();
 
