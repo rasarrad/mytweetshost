0e8943f47cebefac08973ef00f8bff66fce33d04
rasarrad <rasarrad@gmail.com>
2020-05-04 14:21:45 +0100

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -961,7 +961,7 @@ function storeInMap(val) {
     rendermap.set(rendermapindex, val);
     rendermapindex = rendermapindex + 1;
 
-    console.log("storeInMap: " + val.id);
+    //console.log("storeInMap: " + val.id);
 }
 
 function renderLink(val, customize) {
@@ -1052,7 +1052,7 @@ function renderLink(val, customize) {
             setTimeout(function(){
                 customizeSingleTweet(totalrenderedtweets);
                 totalrenderedtweets = totalrenderedtweets + 1;
-            }, 30);
+            }, 250);
         }
     }
     else {
