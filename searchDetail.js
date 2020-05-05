a5da7bbb79e2fad97266c9676b84c7e8615ca5fd
rasarrad <rasarrad@gmail.com>
2020-05-05 09:19:20 +0100

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -884,7 +884,7 @@ var getInformation = function(wasfiltered, valid, flag) {
                     if (dofiltertextfinal && dofilterdate1final && dofiltertagfinal && dofilterdate2final
                         && dofilterauthorfinal && dofiltercatfinal && dofiltertypefinal && dofilterclassiffinal && doShowDeletedLink) {
                         
-                            storeInMap(val);
+                            renderLink(val);
                     }   
     
                     if (val.id == 0) {
