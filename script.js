59481892c54cd8b965928b21e2912c2b61b09a45
rasarrad <rasarrad@gmail.com>
2020-05-07 10:59:24 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -273,10 +273,10 @@ $( document ).ready(function() {
 
         if (scrollLastPos > $(window).scrollTop()) {
             $(".menuhide").css("position", "fixed");
-            $(".menuhide").fadeIn(300);
+            $(".menuhide").slideDown(300);
         }
         else {
-            $(".menuhide").fadeOut(300, function() {
+            $(".menuhide").slideUp(300, function() {
                 $(".menuhide").css("position", "absolute");
             });
         }
