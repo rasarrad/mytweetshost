3d7c1a04790077fb1338eaf46defc5e7284c6d8e
rasarrad <rasarrad@gmail.com>
2020-05-08 17:22:36 +0100

    aaaaaaaa

--- add.js
+++ add.js
@@ -99,8 +99,8 @@ console.log("ssss: " + type)
 
             url = text.substring(text.indexOf('https://www.youtube'), text.indexOf('frameborder') - 2); 
             
-            youtubeId = text.substring(text.indexOf('embed') + 6, text.indexOf('embed') + 17);
-            alert("-" + youtubeId + -"");
+            youtubeId = text.substring(text.indexOf('embed') + 6, text.indexOf('embed') + 16);
+            alert("-" + youtubeId + "-");
             urldirect = "https://www.youtube.com/watch?v=" + youtubeId; 
 
             text = "\"<div class='contentin pobj' id='contentin" + nextid + "' style='background: url(https://img.youtube.com/vi/" 
@@ -158,9 +158,9 @@ console.log("ssss: " + type)
             
             urldirect = text; 
             
-            youtubeId = text.substring(text.indexOf('watch?v=') + 8, text.indexOf('watch?v=') + 19);
+            youtubeId = text.substring(text.indexOf('watch?v=') + 8, text.indexOf('watch?v=') + 18);
 
-            alert("-" + youtubeId + -"");
+            alert("-" + youtubeId + "-");
 
             text = "\"<div class='contentin pobj' id='contentin" + nextid + "' style='background: url(https://img.youtube.com/vi/" 
                     + youtubeId  + "/0.jpg); background-size: 100%;'><i class='logo fa fa-youtube-play'></i></div>\""; 
