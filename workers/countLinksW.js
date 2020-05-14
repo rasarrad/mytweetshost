02b0d7d4674ac61eb909c9ce7ad817862dfa277e
rasarrad <rasarrad@gmail.com>
2020-05-14 10:29:08 +0100

    aaaaaaaa

--- workers/countLinksW.js
+++ workers/countLinksW.js
@@ -1,12 +1,8 @@
 var currTimer = 200;
 
 self.addEventListener("message", function(e) {
-  console.log(e.data.args);
+  console.log(e);
 
-  var aaa = JSON.parse(e.data.args);
-
-  console.log("------------");
-  console.log(aaa.length);
 }, false);
 
 function timedCount() {
