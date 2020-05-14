b378cd044800ab49c8a78ee0b8eca5f6467875b1
rasarrad <rasarrad@gmail.com>
2020-05-14 16:48:30 +0100

    aaaaaaaa

--- workers/countLinksW.js
+++ workers/countLinksW.js
@@ -39,12 +39,33 @@ var total_h = 0;
 var total = 0;   
 
 function processCount(inputArray, showDeleted) {
+  
+/*     setTimeout(function() {     
+        var result = {};
+
+        self.postMessage({ "finnish": "yes", "result": result});
+    }, 8500); */
 
     console.log(inputArray)
     for (var i = 0; i < inputArray.length; i++) {
         var val = inputArray[i];
         console.log(val.id)
+        if (val.deleted != "yes") {
+            
 
+            console.log("aaaaa 22222")
+            if (val.type == "T") {
+                total_t = total_t + 1;
+            }
+            else if (val.type == "Y") {
+                total_y = total_y + 1;
+            }
+            else {
+                total_h = total_h + 1;
+            }
+            total = total + 1;
+            console.log("aaaaa 333333")
+        }   
     }
 } 
 
