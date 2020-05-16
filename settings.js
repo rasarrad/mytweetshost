01fc0fe9d6d3874835cf9601b861337f74365dc1
rasarrad <rasarrad@gmail.com>
2020-05-16 14:35:24 +0100

    aaaaaaaa

--- settings.js
+++ settings.js
@@ -1003,6 +1003,7 @@ function toggleShowDeleted() {
     if (typeof(timeoutWorker) !== "undefined") {
         console.log("timeoutWorker NOT undefined");
             timeoutWorker.terminate();
+            timeoutWorker = undefined;
     }
 
 
