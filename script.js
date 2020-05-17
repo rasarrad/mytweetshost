aed639cc772c545834fff9e82681fa3faeb98b23
rasarrad <rasarrad@gmail.com>
2020-05-17 12:08:39 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -2137,6 +2137,7 @@ function createCookie2(id, name, value, obj) {
             document.cookie = id + "haschanges=yes; path=/";
         }
         else {
+            alert(4566788)
             document.cookie = id + "haschanges=; path=/";
         }
     }
