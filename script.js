84949b7ae7394b0a384f286b459c7876d1b4f2c8
rasarrad <rasarrad@gmail.com>
2020-05-18 12:33:05 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -2340,8 +2340,14 @@ function readCookie(name) {
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) 
-            return c.substring(nameEQ.length, c.length) == "NULL" ? null : c.substring(nameEQ.length, c.length);
+        if (c.indexOf(nameEQ) == 0) {
+            if (c.substring(nameEQ.length, c.length) == "NULL")
+                return null; 
+            else if (c.substring(nameEQ.length, c.length) == "")
+                return " "
+            else 
+                return c.substring(nameEQ.length, c.length)
+        }
     }
     return null;
 }
