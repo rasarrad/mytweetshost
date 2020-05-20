7cf6f0bd7777a6d839ab51bd18df5859a62090dc
rasarrad <rasarrad@gmail.com>
2020-05-20 16:48:50 +0100

    aaaaaaaa

--- change.js
+++ change.js
@@ -357,9 +357,9 @@ function processCountBlock(hasAnyLinkChange, execParamId) {
                     else {
                         val.deletedOri = val.deleted;
                         val.tagsOri = val.tags;
-                        if (id="32")
-                                console.log("2-" + val.categoriesOri + "-")
                         val.categoriesOri = val.categories;
+                        if (id=="32")
+                                console.log("2-" + val.categoriesOri + "-")
                         val.infoOri = val.info;
                         val.classifOri = val.classif;
                         val.authorOri = val.author;
