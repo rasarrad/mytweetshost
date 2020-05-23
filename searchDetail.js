e87e65a9bdd8c3ffd13f7367d5e40a9ed05f0967
rasarrad <rasarrad@gmail.com>
2020-05-23 15:21:22 +0100

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -115,7 +115,7 @@ function changecriteria(e, obj, tableparam, flag) {
             }, 600);
 
             table.find(".togglepos").css("position", ""); 
-            table.find(".titletext").fadeOut(300); 
+            table.find(".titletext").hide(); 
             table.css('transition', 'max-height 0.7s');
     
             if (table.attr("cmaxheight")) {
@@ -151,8 +151,8 @@ function changecriteria(e, obj, tableparam, flag) {
                     searchbutton.css("top", (table.offset().top + 15 + offset) + "px");
                     searchbutton.css('transition', 'all .6s ease-in');
 
-                    table.find(".titletext").fadeIn(300);
                     setTimeout(function() { 
+                        table.find(".titletext").fadeIn(500);
                         dblTapFlag = false;
                     }, 600);
                     
@@ -161,7 +161,6 @@ function changecriteria(e, obj, tableparam, flag) {
             else {
                 setTimeout(function() { 
                     //cnonsole.log(offset);
-                    table.find(".titletext").fadeIn(300);
                     if (table.attr("cheight"))
                         offset = offset + Number(table.attr("cheight"));
                     
@@ -171,6 +170,7 @@ function changecriteria(e, obj, tableparam, flag) {
                     searchbutton.css("top", (table.offset().top + 15 + offset) + "px");
 
                     setTimeout(function() { 
+                        table.find(".titletext").fadeIn(500);
                         dblTapFlag = false;
                     }, 600);
 
@@ -179,7 +179,7 @@ function changecriteria(e, obj, tableparam, flag) {
         }
     }
     else {
-        table.find(".titletext").fadeOut(300);
+        table.find(".titletext").hide();
         table.css('transition', 'max-height 0.7s');
         table.css('max-height', setHeight);
         table.find('.sectionedittd i').addClass('fa-angle-down').removeClass('fa-angle-up').css("top", iTop);
@@ -190,9 +190,9 @@ function changecriteria(e, obj, tableparam, flag) {
         setTimeout(function() { 
             searchbutton.css("left", "18px");
             searchbutton.css('transition', 'all .6s ease-in');
-            table.find(".titletext").fadeIn(300);
             setTimeout(function() { 
                 table.find(".togglepos").css("position", "absolute");
+                table.find(".titletext").fadeIn(500);
                 dblTapFlag = false;
             }, 600);
 
