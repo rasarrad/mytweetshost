2b51f62a2313a1b6d4840677492c076b99ac59a6
rasarrad <rasarrad@gmail.com>
2020-05-24 11:07:28 +0100

    aaaaaaaa

--- settings.js
+++ settings.js
@@ -68,7 +68,6 @@ function fixfocus(el, flag, otherproperty)
         newel.css("transition", property + " 0.3s").css(property, "#");
         setTimeout(function(){
             console.log(newel)
-            newel.css(property, "").css("transition", "");
         }, 400);  
     }
 }
