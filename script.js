f1058c2e0baebda2d5445147d40244643640d90c
rasarrad <rasarrad@gmail.com>
2020-05-25 17:12:51 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -2796,7 +2796,7 @@ function clickMenuTooltip(obj, e) {
 
     if (obj)
         fixfocus(obj);
-    console.log($(obj).parent())
+    console.log($(obj))
 alert( $(obj).parent().attr("title"))
     showTooltip(event, this, true, $(obj).parent().attr("title"))
 }
