21b605de35ccae35eec0f23ea82a8976b5f2dccd
rasarrad <rasarrad@gmail.com>
2020-06-06 18:08:46 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -65,16 +65,8 @@ $( document ).ready(function() {
 
     isMobile = window.mobileAndTabletCheck();
 
-    var cookies = document.cookie.split(";");
 
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-
-
+    
     // START do texto das categorias
     var catschanged = readCookie("cat-cli");
     if (catschanged) {
