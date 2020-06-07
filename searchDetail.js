3569512b8701f1a2411114fc8455534a753b7be1
rasarrad <rasarrad@gmail.com>
2020-06-07 19:00:11 +0100

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -97,7 +97,6 @@ function changecriteria(e, obj, tableparam, flag) {
 
             $('#searchpopup').find("table:not(.buttonstable)").each( function( index, element ) {
                 var othertable = $(element);
-                othertable.css('transition', 'max-height .7s');
                 othertable.css('max-height', setHeight);
                 othertable.find('.sectionedittd i').addClass('fa-angle-down').removeClass('fa-angle-up').css("top", iTop);
 
@@ -119,17 +118,13 @@ function changecriteria(e, obj, tableparam, flag) {
             table.css('transition', 'max-height 1.7s');
     
             if (table.attr("cmaxheight")) {
-                table.css('max-height', '2000px');
-/*                 if ($('body').hasClass('big')) {
+                if ($('body').hasClass('big')) {
                     table.css('max-height', table.attr("cmaxheightbig"));
                 }
                 else {
                     table.css('max-height', table.attr("cmaxheight"));
-                } */
+                }
             }   
-            else {
-                table.css('max-height', "fit-content");
-            }
     
             table.find('.sectionedittd i').addClass('fa-angle-up').removeClass('fa-angle-down').css("top", "-4px");
             
