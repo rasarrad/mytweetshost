97404f7335827e984cb31867d9e99412ff99a8e6
rasarrad <rasarrad@gmail.com>
2020-06-07 15:00:54 +0100

    aaaaaaaa

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ $( document ).ready(function() {
     var z = 0;
     for (var j= 0; j < 10000; j++) {
 
-        if (localStorage['movie'+ i])
+        if (localStorage['movie'+ j])
             z++;
     }
 
