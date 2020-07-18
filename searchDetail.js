20522619a978666833e98217d49f7244fe967bbb
rasarrad <rasarrad@gmail.com>
2020-07-18 19:19:03 +0100

    aaaaaaaa

--- searchDetail.js
+++ searchDetail.js
@@ -943,7 +943,7 @@ function renderLink(val, flag) {
             {
               conversation : 'none',    // or all
               cards        : 'hidden',  // or visible
-              linkColor    : '#cc0000', // default is blue
+              linkColor    : 'red', // default is blue
               theme        : 'dark'    // or dark
             })
           .then (function (el) {
