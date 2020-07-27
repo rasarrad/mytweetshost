4ec1d8b37d4d0f404b8bb2f11a388fcb8e123712
rasarrad <rasarrad@gmail.com>
2020-07-27 11:51:48 +0100

    aaaaaaaa

--- workers/countLinksW.js
+++ workers/countLinksW.js
@@ -16,7 +16,10 @@ self.addEventListener("message", function(e) {
         result.total_t = total_t;
         result.total_h = total_h;
         result.total = total;
+        console.log(111111111)
+        console.log(result.tagsmap)
         
+
         self.postMessage({ "finnish": "yes", "result": result});
 
         resetVars();
