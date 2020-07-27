a1248f3df6c9c5189dfec46317b45a4a8281f032
rasarrad <rasarrad@gmail.com>
2020-07-27 13:56:44 +0100

    aaaaaaaa

--- change.js
+++ change.js
@@ -778,9 +778,11 @@ function autocomplete(inp, arr) {
         /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(a);
         /*for each item in the array...*/
+        var index = 0;
         for (i = 0; i < arr.length; i++) {
           /*check if the item starts with the same letters as the text field value:*/
           if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            index++;
             /*create a DIV element for each matching element:*/
             b = document.createElement("DIV");
             /*make the matching letters bold:*/
@@ -805,8 +807,7 @@ function autocomplete(inp, arr) {
             });
           }
         }
-        console.log(arr.length)
-        if (arr.length == 1) {
+        if (index == 1) {
             console.log(123)
 
             $(inp).val($(a).find("input").val());
